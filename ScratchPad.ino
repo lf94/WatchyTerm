97e5e17ee421ddd5cba9f0378e990596f7f34259
Lee <inbox@leefallat.ca>
2021-07-18 11:11:17 -0400

    Rewrite everything

--- ScratchPad.ino
+++ ScratchPad.ino
@@ -35,44 +35,95 @@
 
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
 
-#define BLEN (1024 * 100)
 #define BG GxEPD_WHITE
 #define FG GxEPD_BLACK
 
+#define TOTAL_LINES 500
+#define MAX_CHARS_PER_LINE 30
+
+// This is font independent! Manually check.
+#define LINES_PER_PAGE 14
+
 struct TextBuffer {
 
-  // A circular buffer to hold all the incoming text.
-  char ring[BLEN];
+  // During receiving, text width is calculated and is split up into lines.
+  char lines[TOTAL_LINES][MAX_CHARS_PER_LINE];
 
-  // The line pointer always points towards the beginning of a line which is
-  // where printing will start from.
-  char *line;
+  uint16_t print_from_line;
 
-  // Points toward the beginning of the next page, which is always the beginning
-  // of a line. Set to NULL when there is no next page.
-  char *nextPage;
-
-  // Essentially the same behavior, just in reverse, as the above.
-  char *prevPage;
+  // Where to start inserting into the buffer, but also do not print past this
+  // point, since it signals the end of the buffer / ring.
+  uint16_t insert_from_line;
 };
 
-TextBuffer text;
-
-void text_buffer_default(TextBuffer* t) {
-  t->line = t->ring;
-  t->prevPage = t->line;
-  t->nextPage = NULL;
+void text_buffer_default(TextBuffer *tb) {
+  tb->print_from_line = 0;
+  tb->insert_from_line = 0;
 }
 
-void text_buffer_page_next(TextBuffer *t) {
-  t->prevPage = t->line;
-  t->line = t->nextPage;
+void text_buffer_insert(TextBuffer* tb, char* text) {
+  // Since we print character-by-character, we prepare this small array to hold
+  // a single character for u8g2.
+  char p[2];
+  p[1] = '\0';
+
+  // Screen is 200x200, small enough to address using 8 bits.
+  uint8_t x = 0;
+
+  // Track where we are in a line.
+  uint8_t at_char = 0;
+    
+  for (char* c = text; c != '\0'; c += 1) {
+    if (x >= DISPLAY_WIDTH) {
+      tb->lines[tb->insert_from_line][at_char] = '\0';
+      x = 0;
+      at_char = 0;
+      tb->insert_from_line += 1;
+    }
+    if (*c == '\n') {
+      tb->lines[tb->insert_from_line][at_char] = '\0';
+      x = 0;
+      at_char = 0;
+      tb->insert_from_line += 1;
+    }
+    
+    p[0] = *c;
+    tb->lines[tb->insert_from_line][at_char] = *c;
+    x += u8g2Fonts.getUTF8Width(p);
+    at_char += 1;
+  }
 }
 
-void text_buffer_page_prev(TextBuffer *t) {
-  t->nextPage = t->line;
-  t->line = t->prevPage;
+void text_buffer_print(TextBuffer* tb)
+{
+  // int16_t tw; // The glyph width is calculated below, so this is not necessary.
+  int16_t ta = u8g2Fonts.getFontAscent();
+  int16_t td = u8g2Fonts.getFontDescent();
+  int16_t th = ta + abs(td);
+
+  display.fillScreen(BG);
+
+  uint16_t x = 0;
+  uint16_t y = th;
+
+  for(
+    uint16_t at_line = tb->print_from_line;
+    y < DISPLAY_HEIGHT
+    && at_line != tb->insert_from_line;
+    at_line += 1,
+    y += th
+  ) {
+    u8g2Fonts.setCursor(x, y);
+    u8g2Fonts.print(&tb->lines[at_line][0]);
+    x += u8g2Fonts.getUTF8Width(&tb->lines[at_line][0]);
+  }
+
+  display.display();
 }
+
+
+
+TextBuffer text_buffer;
 
 void setup()
 {
@@ -86,69 +137,20 @@ void setup()
   u8g2Fonts.setBackgroundColor(BG);
   u8g2Fonts.setFont(u8g2_font_ncenB10_tf);
 
-  text_buffer_default(&text);
+  text_buffer_default(&text_buffer);
 
-  sprintf(text.ring, "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14eeeeeeeeeeeeeeaoteuaoehusaoheischaosrcuhasoetuh\n15\n16\n17\n18\n19\20");
-  text.nextPage = printt(&text);
-  text_buffer_page_next(&text);
-  text.nextPage = printt(&text);
-  text_buffer_page_prev(&text);
-  text.nextPage = printt(&text);
-  text_buffer_page_next(&text);
-  text.nextPage = printt(&text);
+  char* text = NULL;
+  sprintf(text, "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14eeeeeeeeeeeeeeaoteuaoehusaoheischaosrcuhasoetuh\n15\n16\n17\n18\n19\20");
+  text_buffer_insert(&text_buffer, text);
+  text_buffer_print(&text_buffer);
   
   // Enable deep sleep wake on any button press
   esp_sleep_enable_ext1_wakeup(BTN_PIN_MASK, ESP_EXT1_WAKEUP_ANY_HIGH);
   esp_deep_sleep_start();
 }
 
+
 void loop()
 {
 
-}
-
-// Returns a pointer to the next page if there is one.
-char* printt(TextBuffer *text)
-{
-  // int16_t tw; // The glyph width is calculated below, so this is not necessary.
-  int16_t ta = u8g2Fonts.getFontAscent();
-  int16_t td = u8g2Fonts.getFontDescent();
-  int16_t th = ta + abs(td);
-
-  uint16_t x = 0;
-  uint16_t y = th;
-  char p[2];
-  p[1] = '\0';
-
-  char *nextPage = NULL;
-
-  display.fillScreen(BG);
-     
-  for(char *c = text->line; *c != '\0'; c++) {
-    if (x >= DISPLAY_WIDTH) {
-      x = 0;
-      y += th;
-    }
-    if (*c == '\n') {
-      x = 0;
-      y += th;
-    }
-    
-    u8g2Fonts.setCursor(x, y);
-    p[0] = *c;
-    u8g2Fonts.print(p);
-    x += u8g2Fonts.getUTF8Width(p);
-
-    if (y >= DISPLAY_HEIGHT) {
-      if (*c == '\n') {
-        c = c + 1;
-      }
-      nextPage = c;
-      break;
-    }
-  }
-
-  display.display();
-
-  return nextPage;
 }
